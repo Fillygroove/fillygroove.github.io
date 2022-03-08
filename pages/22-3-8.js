b26c8b09cc9941881966361a8cc15ed1c9c527ff
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2022-03-08 18:43:52 -0500

    8th of March, 2022

---
+++ pages/22-3-8.js
@@ -0,0 +1,34 @@
+let data = {
+    name: `8th of March, 2022`,
+    main: [
+        `freef`,
+        `So I had to add an hr tag to the constructor so that the titles are bearable to actually look at. I did this to Purple, too.`,
+        `Tomorrow I'll listen to these albums! "NEWS AT 11", "; [Seicolon]", "Neon Tracer", "Cubism", "Artificial OS", "Metah", "I Don’t Like Shit, I Don’t Go Outside", "Empty Hiding World" and "COMPUTER IMAGINA"`,
+        `actually i should make a list i'll do that real quick, you should check the f12 console!`,
+        () => {
+            console.log('hey! this is a test to make sure I remembered my own syntax. I did so that makes me happy.');
+            addList([
+                "NEWS AT 11", 
+                "; [Seicolon]", 
+                "Neon Tracer", 
+                "Cubism", 
+                "Artificial OS", 
+                "Metah", 
+                "I Don’t Like Shit, I Don’t Go Outside", 
+                "Empty Hiding World",
+                "COMPUTER IMAGINA"
+            ]);
+        },
+        `Originally I called the function makeList but addList makes more sense.`,
+        `One day I should make a quine with this format. Maybe I'll make a function for that.`,
+        `I should add images, too. That'll make things easier. Oh well, future me's problem.`,
+        `One thing I'm thinking is that I'm sort of screaming into an internet void; In reality nobody's probably going to really care but I do, to be honest.`,
+        `I'll upload Volume 2 of Pokemon Purple, I found where the journal for that was.`,
+        `Right now I'm listening to "Black Country, New Road". It's a good album!`,
+        `I'll add a sort of navigator. It'll have nothing for now but I'll update as much as I can!`
+    ],
+    nav: {
+        prev: 'None',
+        next: 'Soon'
+    }
+};
